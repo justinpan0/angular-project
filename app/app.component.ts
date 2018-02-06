a07c1dd3588eac2a78a534e2a17a906713445c3a
zimengpan <zimengpan@gmail.com>
2018-02-06 17:56:24 -0500

    Inital commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  styles: [`
-    
-  `],
-  template: `
-    <div class="jumbotron text-center">
-      <h1>The App Lives!</h1>
-      <p>{{ message }}</p>
-    </div>
-  `
+    selector: 'my-app',
+    template: `
+        <div class="well">
+        <h3>Example</h3>
+            <input #textbox type="text" ngModel="fontcolor" required>
+            <button (click)="logText(textbox.value)">Update Color</button>
+        <p #part1 class="box" [style.color]="fontcolor">
+            Hello World! 
+        </p>
+            <h4>Log <button (click)="log=''">Clear</button></h4>
+            <pre>{{log}}</pre>
+        </div>`,
+    styles: ['.box {max-width: 450px; padding: 5px; }']
 })
 export class AppComponent {
-  message = 'This is the sample message.';
+
+    fontcolor = 'Red';
+    log='';
+
+    logText(value: string): void {
+        this.fontcolor = value;
+        this.log += `Text changed to '${this.fontcolor}'\n`;
+    }
 }
--- dist/app.component.js
+++ dist/app.component.js
@@ -5,23 +5,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var core_1 = require('@angular/core');
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.message = 'This is the sample message.';
+        this.fontcolor = 'Red';
+        this.log = '';
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            styles: ["\n    \n  "],
-            template: "\n    <div class=\"jumbotron text-center\">\n      <h1>The App Lives!</h1>\n      <p>{{ message }}</p>\n    </div>\n  "
-        }), 
-        __metadata('design:paramtypes', [])
-    ], AppComponent);
+    AppComponent.prototype.logText = function (value) {
+        this.fontcolor = value;
+        this.log += "Text changed to '" + this.fontcolor + "'\n";
+    };
     return AppComponent;
 }());
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: "\n        <div class=\"well\">\n        <h3>Example</h3>\n            <input #textbox type=\"text\" ngModel=\"fontcolor\" required>\n            <button (click)=\"logText(textbox.value)\">Update Color</button>\n        <p #part1 class=\"box\" [style.color]=\"fontcolor\">\n            Hello World! \n        </p>\n            <h4>Log <button (click)=\"log=''\">Clear</button></h4>\n            <pre>{{log}}</pre>\n        </div>",
+        styles: ['.box {max-width: 450px; padding: 5px; }']
+    })
+], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
